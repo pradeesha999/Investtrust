8d240ec94b855b59e4b15a6e1fc260302f0e21de
Pradeesha <pradeeshakantha@gmail.com>
2026-03-29 10:52:24 +0530

    Initial Commit

---
+++ Investtrust/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Investtrust
+//
+//  Created by COBSCCOMP242P-052 on 2026-03-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
